6a4a9eb7bfc5bc515cea8fff5363ae799fdd66c5
diinfsch <163923016+diinfsch@users.noreply.github.com>
2025-04-16 18:48:53 +0200

    initial migration (#1)
    Co-authored-by: schulzesttsi <steffen.schulze@t-systems.com>

---
+++ tests/did_test.go
@@ -0,0 +1,85 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/eclipse-xfsc/did-core"
+	"github.com/spf13/viper"
+)
+
+const didJSON = `{
+	"@context": [
+    "https://www.w3.org/ns/did/v1",
+    "https://w3id.org/security/suites/jws-2020/v1",
+    "https://w3id.org/security/suites/ed25519-2020/v1"
+  ],
+  "id": "did:example:123456789abcdefghi",
+  
+  "verificationMethod": [{
+    "id": "did:example:123#_Qq0UL2Fq651Q0Fjd6TvnYE-faHiOpRlPVQcY_-tA4A",
+    "type": "JsonWebKey2020", 
+    "controller": "did:example:123",
+    "publicKeyJwk": {
+      "crv": "Ed25519", 
+      "x": "VCpo2LMLhn6iWku8MKvSLg2ZAoC-nlOyPVQaO3FxVeQ", 
+      "kty": "OKP", 
+      "kid": "_Qq0UL2Fq651Q0Fjd6TvnYE-faHiOpRlPVQcY_-tA4A" 
+    }
+  }, {
+    "id": "did:example:123#_Qq0UL2Fq651Q0Fjd6TvnYE-faHiOpRlPVQcY_-tA4Afff",
+    "type": "JsonWebKey2020", 
+    "controller": "did:example:123",
+    "publicKeyJwk": {
+      "crv": "Ed25519", 
+      "x": "VCpo2LMLhn6iWku8MKvSLg2ZAoC-nlOyPVQaO3FxVeQ", 
+      "kty": "OKP", 
+      "kid": "_Qq0UL2Fq651Q0Fjd6TvnYE-faHiOpRlPVQcY_-tA4A" 
+    }
+  }]
+  }`
+
+func TestDidDocumentParsing(t *testing.T) {
+	didDoc, err := did.ParseDidDocument(didJSON)
+
+	if err != nil || didDoc == nil {
+		t.Error(err)
+	}
+
+	if didDoc.VerificationMethod[0].PublicKeyJwk == nil {
+		t.Error()
+	}
+}
+
+func TestGetKeys(t *testing.T) {
+	didDoc, err := did.ParseDidDocument(didJSON)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	set := didDoc.GetPublicKeys()
+
+	if set.Len() != 2 || set == nil {
+		t.Error()
+	}
+
+}
+
+func TestDidResolver(t *testing.T) {
+	viper.SetDefault("DID_RESOLVER", "https://dev.uniresolver.io")
+	didString := "did:key:zDnaerDaTF5BXEavCrfRZEk316dpbLsfPDZ3WJ5hRTPFU2169"
+
+	d, err := did.Resolve(didString)
+
+	if d == nil || err != nil {
+		t.Error()
+	}
+
+	if d.Id != didString {
+		t.Error()
+	}
+
+	if d.VerificationMethod != nil && len(d.VerificationMethod) == 0 {
+		t.Error()
+	}
+}
---
+++ types/did.go
@@ -0,0 +1,43 @@
+package types
+
+import (
+	"encoding/json"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/sirupsen/logrus"
+)
+
+type DidVerificationMethod struct {
+	Id           string  `json:"id"`
+	Controller   string  `json:"controller"`
+	Type         string  `json:"type"`
+	PublicKeyJwk jwk.Key `json:"publicKeyJwk,omitempty"`
+}
+
+type DidDocument struct {
+	Id                 string                  `json:"id"`
+	Controller         string                  `json:"controller"`
+	VerificationMethod []DidVerificationMethod `json:"verificationMethod"`
+}
+
+func (d *DidDocument) GetPublicKeys() jwk.Set {
+	METHOD_TYPE := "JsonWebKey2020"
+	keys := jwk.NewSet()
+	for _, item := range d.VerificationMethod {
+		if item.Type == METHOD_TYPE {
+			jwkJson, err := json.Marshal(item.PublicKeyJwk)
+			if err == nil {
+				key, err := jwk.ParseKey(jwkJson)
+				key.Set(jwk.KeyIDKey, item.Id)
+				if err == nil {
+					keys.AddKey(key)
+				} else {
+					logrus.Info(item.Id + "is not valid JWK.")
+				}
+			} else {
+				logrus.Info(item.Id + "is not valid JWK Definition.")
+			}
+		}
+	}
+	return keys
+}
